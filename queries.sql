91ff1fa4f498620b50c34a09d518beaea40bd4ed
snkorolev1976-maker <snkorolev1976@gmail.com>
2026-09-20 07:15:36 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -64,3 +64,83 @@ GROUP BY
 ORDER BY
     day_of_week,
     seller;
+
+
+/* Запрос возвращает количество покупателей в различных возврастных группах*/
+
+SELECT
+    CASE
+        WHEN age BETWEEN 16 AND 25 THEN '16-25'
+        WHEN age BETWEEN 26 AND 40 THEN '26-40'
+        WHEN age > 40 THEN '40+'
+    END AS age_category,
+    COUNT(c.customer_id) AS age_count
+FROM customers AS c
+GROUP BY
+    1
+ORDER by
+1;
+
+
+/*Данные по количеству уникальных покупателей и выручке, которую они принесли*/
+
+SELECT
+    TO_CHAR(s.sale_date, 'YYYY-MM') AS selling_month,
+    COUNT(DISTINCT s.customer_id) AS total_customers,
+    FLOOR(SUM(s.quantity * p.price)) AS income
+FROM customers AS c
+JOIN sales AS s
+    ON c.customer_id = s.customer_id
+JOIN products AS p
+    ON s.product_id = p.product_id
+GROUP BY
+    1
+ORDER BY
+    1;
+
+
+/* отчет о покупателях, первая покупка которых была в ходе проведения акций*/
+
+WITH rn_sales AS (
+    SELECT
+        customer_id,
+        sale_date,
+        product_id,
+        sales_person_id,
+        ROW_NUMBER() OVER (
+            PARTITION BY customer_id
+            ORDER BY sale_date, sales_id
+        ) AS rn
+    FROM sales
+)
+SELECT
+    CONCAT(c.first_name, ' ', c.last_name) AS customer,
+    rnc.sale_date AS sale_date,
+    CONCAT(e.first_name, ' ', e.last_name) AS seller
+FROM rn_sales AS rnc
+JOIN customers AS c
+    ON c.customer_id = rnc.customer_id
+JOIN products AS p
+    ON p.product_id = rnc.product_id
+JOIN employees AS e
+    ON e.employee_id = rnc.sales_person_id
+WHERE
+    p.price = 0
+    AND rnc.rn = 1
+ORDER BY
+    1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
